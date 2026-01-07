20df6f6bb1a60faf3f4c6ccd72013360a8c76680
Ishant <ishant37767@gmail.com>
2026-01-07 17:30:20 +0530

    add-new-task form work complete

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Hero Title and Button logic
+
 let Clicktoadd=  document.querySelector('.hero-button');
 
 let toggleAddNewTask=()=>{
@@ -8,13 +10,82 @@ let toggleAddNewTask=()=>{
 
 Clicktoadd.addEventListener('click',toggleAddNewTask);
 
+//Header Date
+let weekdays=['Sunday', 'Monday','tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+let monthnames=['January','February','March','April','May','June','July','August','September','October','November','December']
+let today = new Date()
+let date=`${weekdays[today.getDay()]}, ${today.getDate()} ${monthnames[today.getMonth()]} ${today.getFullYear()}`
+document.querySelector(".header-right").innerText=`${date}`
+
+
+//Task Input form logic
 
 let task= document.getElementById("task")
 let day=document.getElementById("day")
 let month=document.getElementById("month")
 let year=document.getElementById("year")
 let add= document.querySelector(".add-button")
+let cancel= document.querySelector('.cancel-button')
 let tasklist=document.getElementById("tasks-list")
+
+
+for(let i=1;i<=31;i++){
+    let dayoption=document.createElement('option');
+    dayoption.setAttribute('value',i)
+    dayoption.innerHTML=i
+    day.append(dayoption)
+}
+
+
+
+monthnames.forEach((value,index)=>{
+    let monthoption=document.createElement('option')
+    monthoption.setAttribute('value',index+1)
+    monthoption.innerHTML=value
+    month.append(monthoption)
+})
+
+for(let i=today.getFullYear();i<(today.getFullYear()+5);i++){
+    let yearoption=document.createElement('option')
+    yearoption.setAttribute('value',i)
+    yearoption.innerHTML=i
+    year.append(yearoption)
+}
+
+let daySelect=(mon)=>{
+    day.innerHTML=` <option value="" disabled selected>Day</option> `
+    if(mon===4 || mon===6 || mon===9 || mon===11){
+        for(let i=1;i<=30;i++){
+            let dayoption=document.createElement('option');
+            dayoption.setAttribute('value',i)
+            dayoption.innerHTML=i
+            day.append(dayoption)
+    }
+        
+    }
+    else if(mon===2){
+        for(let i=1;i<=29;i++){
+            let dayoption=document.createElement('option');
+            dayoption.setAttribute('value',i)
+            dayoption.innerHTML=i
+            day.append(dayoption)
+        }
+    }
+    else{
+        for(let i=1;i<=31;i++){
+            let dayoption=document.createElement('option');
+            dayoption.setAttribute('value',i)
+            dayoption.innerHTML=i
+            day.append(dayoption)
+        }
+    }
+
+}
+
+
+
+
+month.addEventListener('change',()=>{daySelect(Number(month.value))})
 
 let taskData=[]
     
@@ -51,8 +122,19 @@ let taskInput=(tasktitle,lastdate)=>{
                         </article>
         `
     tasklist.append(newtask)
+
+   
     
 }
 
-add.addEventListener('click',()=>{taskInput(task.value,`${day.value} ${month.value} ${year.value}`)})
+add.addEventListener('click',()=>{taskInput(task.value,`${day.value} - ${month.value} - ${year.value}`)})
+
+let clearform=()=>{
+    task.value=""
+    year.value=""
+    month.value=""
+    day.value=""
+}
+
+cancel.addEventListener('click',clearform)
 
